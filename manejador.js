0de1cde65d0e6fc91e05afecd9bd6668d7f19384
Your Nameclear <you@example.com>
2021-06-16 17:02:04 -0300

    my first commit

---
+++ manejador.js
@@ -0,0 +1,11 @@
+let nombre = prompt(" escribe aquí tu nombre");
+
+let apellido = prompt("escribe aquí tu apellido");
+
+alert(
+  nombre +
+    " " +
+    apellido +
+    " " +
+    "va a ser el mejor alumno de la clase de coder house"
+);
